79571eca86041036c18eeb6ac3f3063093f3f4f8
Denis Naughton <mjolinor75@hotmail.com>
2021-02-02 00:53:49 -0500

    finished consec v2

--- consec.rb
+++ consec.rb
@@ -1,9 +1,7 @@
 #Given an unsorted array of numbers ,return 1 if all the numbers are consecutive else return -1.
 def consec(arr)
 
-	(0...arr.length-1).each do |idx|
-		return false if arr[idx] > arr[idx + 1] 
-	end
+	(0...arr.length-1).each { |idx| return false if arr[idx] > arr[idx + 1] }
 
 	true
 
